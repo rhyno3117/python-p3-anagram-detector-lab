7e82702f79c71285dfd4e7a60a1a09804304e9b1
Ryan Meyers <rhynogolfer42@gmail.com>
2023-09-12 16:02:34 -0600

    Adds myfile.md

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        word_letters = [letter for letter in self.word]
+
+        empty_list = []
+
+        for each_word in list:
+            list_word_letters = [letter for letter in each_word]
+            if(sorted(word_letters) == sorted(list_word_letters)):
+                empty_list.append(each_word)
+
+        return empty_list
